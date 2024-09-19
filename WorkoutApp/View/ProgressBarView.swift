944e0158754b2b3b0e4add232cfa9866228d7b54
Yegor <ncsftw@gmail.com>
2024-09-19 13:37:34 -0700

    Add .gitignore file

---
+++ WorkoutApp/View/ProgressBarView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressBarView.swift
+//  WorkoutApp
+//
+//  Created by Yegor Yeryomenko on 2024-09-19.
+//
+
+import SwiftUI
+
+struct ProgressBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressBarView()
+}
